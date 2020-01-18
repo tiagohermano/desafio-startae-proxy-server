21d730c557c89fcccfd7f905d4c06ed20fb4cc28
Tiago Hermano <tiagofhermano@gmail.com>
2020-01-18 01:36:10 -0300

    testing heroku deployment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', async (req, res) => {
+app.use('/', async (req, res) => {
   T.get('statuses/user_timeline', { screen_name: 'late4party', count: 5, trim_user: true, exclude_replies: true }, function(err, data, response) {
     res.send(data);
   });
