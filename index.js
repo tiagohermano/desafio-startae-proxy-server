8f990bc2ca4f380442cd086a70fecdc9d14d4bd4
Tiago Hermano <tiagofhermano@gmail.com>
2020-01-18 22:24:01 -0300

    fix root route's path definition

--- index.js
+++ index.js
@@ -21,17 +21,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/tweets/', async (req, res) => {
-  const { username, tweetsAmount } = req.query;
-  T.get('statuses/user_timeline', { screen_name: username, count: tweetsAmount, trim_user: true, exclude_replies: true }, function(err, data, response) {
-    if(err) res.send(err);
-    res.send(data);
-  });
+app.use('/', (req, res) => {
+  res.send('Desafio Frontend Startae Proxy Server for Twitter API requests.');
 });
 
-app.use('/', async (context, next) => {
-  context.body = 'Desafio Frontend Startae Proxy Server for Twitter API requests.';
-  return next();
+app.use('/tweets/', async (req, res) => {
+  const { username, tweetsAmount } = req.query;
+  T.get('statuses/user_timeline', { screen_name: username, count: tweetsAmount, exclude_replies: true }, function(err, data, response) {
+    if(err) return res.send(err);
+    res.send(data);
+  });
 });
 
 app.listen(process.env.PORT || 7777, () => console.log('SERVER RUNNING ON localhost:7777'));
